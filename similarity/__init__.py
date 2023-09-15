b9808b09d9586e20903886eff17ab3d996d8bf5a
Baba Ramdev <subhamdas1801@gmail.com>
2023-09-15 16:12:46 +0530

    add: String similarity methods added by @Pinaki.

---
+++ similarity/__init__.py
@@ -0,0 +1,5 @@
+
+# Importing the self Class from the directory
+from .cosine_text_match import CosineSimilarity
+from .jaccard_sim import JaccardSimilarity
+from .lavenshtein import LevenshteinDistance
