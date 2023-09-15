29f537203b74d228d7b0f294e9cde821231d5df6
Baba Ramdev <subhamdas1801@gmail.com>
2023-09-15 22:31:09 +0530

    add: ssim method added.

--- similarity/__init__.py
+++ similarity/__init__.py
@@ -1,5 +1,6 @@
 
 # Importing the self Class from the directory
-from .cosine_text_match import CosineSimilarity
+from .cosine_match import CosineSimilarity
 from .jaccard_sim import JaccardSimilarity
 from .lavenshtein import LevenshteinDistance
+from .ssim import StructuralSimilarityIndex
